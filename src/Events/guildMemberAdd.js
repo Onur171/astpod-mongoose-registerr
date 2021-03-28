383950ac91ef3464f7238188069e28a9ff36bfdb
Astpod <49484285+Astpod@users.noreply.github.com>
2021-03-28 18:45:08 +0300

    Update guildMemberAdd.js

--- src/Events/guildMemberAdd.js
+++ src/Events/guildMemberAdd.js
@@ -1,5 +1,5 @@
-const moment = require("moment"),
-moment.locale("tr"),
+const moment = require("moment");
+moment.locale("tr");
 const Base = require("../Base/Event"),
     { Client } = global,
     cfg = require('../../config.json');
